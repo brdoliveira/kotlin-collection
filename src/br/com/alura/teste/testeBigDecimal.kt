6327601c60238d5d6983c4d04abb38d178897464
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 00:07:48 -0300

    FEAT(testeBigDecimal): Criando testeBigDecimal

--- src/br/com/alura/teste/testeBigDecimal.kt
+++ src/br/com/alura/teste/testeBigDecimal.kt
@@ -1,4 +1,49 @@
 package br.com.alura.teste
 
-class testeBigDecimal {
+import br.com.alura.utils.*
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun testeBigDecimal() {
+    val salarios1 = Array<BigDecimal>(5){ BigDecimal.ZERO}
+    salarios1[0] = "1500.55".toBigDecimal()
+    salarios1[1] = "2000.00".toBigDecimal()
+    println(salarios1.contentToString())
+
+    val salarios2 = bigDecimalArrayOf("1500.55","2000.00","10000.00")
+    println(salarios2.contentToString())
+
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento: Array<BigDecimal> = salarios2
+        .map { salario -> calculaAumentoRelativo(salario,aumento)
+        }.toTypedArray()
+    println(salariosComAumento)
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto inicial com os salários: $gastoInicial")
+
+    val meses = "6".toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial){ acumulador, salario ->
+        acumulador + (salario * meses).setScale(2,RoundingMode.UP)
+    }
+    println("Gasto total com os salários: $gastoTotal")
+
+    val mediaMaiores = salariosComAumento
+                .sorted()
+                .takeLast(3)
+                .toTypedArray()
+                .media()
+    println("Media maiores: $mediaMaiores")
+
+    val mediaMenores = salariosComAumento
+        .sorted()
+        .take(3)
+        .toTypedArray()
+        .media()
+    println("Media menores: $mediaMenores")
+}
+
+fun calculaAumentoRelativo(salario: BigDecimal, aumento: BigDecimal): BigDecimal{
+    return if(salario < "5000".toBigDecimal()) salario + "500".toBigDecimal()
+    else (salario * aumento).setScale(2,RoundingMode.UP)
 }
