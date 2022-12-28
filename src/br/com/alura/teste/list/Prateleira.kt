6f522355e9d1c8677368139210578e17885e9a8a
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 22:58:25 -0300

    FEAT(Prateleira): Criando classe Prateleira

--- src/br/com/alura/teste/list/Prateleira.kt
+++ src/br/com/alura/teste/list/Prateleira.kt
@@ -1,3 +1,14 @@
 package br.com.alura.teste.list
 
-data class Prateleira()
+data class Prateleira (
+    val genero: String,
+    val livros: List<Livro>
+) {
+    fun organizarPorAutor(): List<Livro> {
+        return livros.sortedBy { it.autor }
+    }
+
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
+    }
+}
