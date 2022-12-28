08cfe91b8b66aa5477833af3d3049a698f9a1820
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 23:00:06 -0300

    FEAT(testeLivrosComAtributoNulo): Criando teste testeLivrosComAtributoNulo

--- src/br/com/alura/teste/list/testeLivrosComAtributoNulo.kt
+++ src/br/com/alura/teste/list/testeLivrosComAtributoNulo.kt
@@ -1,2 +1,9 @@
 package br.com.alura.teste.list
 
+fun testeLivrosComAtributoNulo(){
+    listaDeLivros
+        .groupBy { it.editora ?: "Editora desconhecida" }
+        .forEach { (editora: String, livros: List<Livro>) ->
+            println("$editora: ${livros.joinToString { it.titulo }}")
+        }
+}
