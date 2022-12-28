3ccd614d53b5fd1ad1c2bc3302ea01c24c0567da
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 22:57:52 -0300

    FEAT(Livro): Criando classe Livro

--- src/br/com/alura/teste/list/Livro.kt
+++ src/br/com/alura/teste/list/Livro.kt
@@ -1,3 +1,12 @@
 package br.com.alura.teste.list
 
-data class Livro()
+data class Livro(
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+): Comparable<Livro> {
+    override fun compareTo(other: Livro): Int {
+        return this.anoPublicacao.compareTo(other.anoPublicacao)
+    }
+}
