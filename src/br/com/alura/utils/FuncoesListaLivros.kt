d71cbaabfe66310be1a184e77b91c34b8b66eb26
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 23:03:28 -0300

    FEAT(FuncoesListaLivros): Criando imprimeComMarcadores

--- src/br/com/alura/utils/FuncoesListaLivros.kt
+++ src/br/com/alura/utils/FuncoesListaLivros.kt
@@ -1,2 +1,12 @@
 package br.com.alura.utils
 
+import br.com.alura.teste.list.Livro
+
+fun List<Livro?>.imprimeComMarcadores() {
+    val textoFormatado = this
+        .filterNotNull()
+        .joinToString(separator = "\n") {
+            " - ${it.titulo} de ${it.autor}"
+        }
+    println(" #### Lista de Livros #### \n$textoFormatado")
+}
