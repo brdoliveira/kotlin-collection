ea9171c8d68c93d8d2cb5712b0c85ba75a7a24ff
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-27 23:00:28 -0300

    FEAT(testeUsoPrateleiras): Criando teste testeUsoPrateleiras

--- src/br/com/alura/teste/list/testeUsoPrateleiras.kt
+++ src/br/com/alura/teste/list/testeUsoPrateleiras.kt
@@ -1,2 +1,13 @@
 package br.com.alura.teste.list
 
+import br.com.alura.utils.imprimeComMarcadores
+
+fun testeUsoPrateleiras(){
+    val prateleira = Prateleira(genero = "Literatura", livros = listaDeLivros)
+
+    val porAutor = prateleira.organizarPorAutor()
+    val porAnoPublicacao = prateleira.organizarPorAnoPublicacao()
+
+    porAutor.imprimeComMarcadores()
+    porAnoPublicacao.imprimeComMarcadores()
+}
