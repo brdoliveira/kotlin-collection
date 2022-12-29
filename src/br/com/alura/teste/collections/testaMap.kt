571ce88caf3877d0f863b7d0ec70bd5ef44cb849
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-28 23:43:17 -0300

    FEAT(testaMap): Criando teste testaMap

--- src/br/com/alura/teste/collections/testaMap.kt
+++ src/br/com/alura/teste/collections/testaMap.kt
@@ -1,2 +1,30 @@
 package br.com.alura.teste.collections
 
+fun testaMap(pedidos: MutableMap<Int, Double>) {
+    println(pedidos)
+
+    val pedido = pedidos[3]
+    pedido?.let {
+        println("pedido $it")
+    }
+
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("numero do pedido: ${p.key}")
+        println("valor do pedido: ${p.value}")
+    }
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    pedidos.remove(3, 100.0)
+    println(pedidos)
+}
