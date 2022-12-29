0217f2159223f7648285f4270de45d9498ed5df0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-28 23:41:14 -0300

    FEAT(BancoDeNomes): Criando classe BancoDeNome

---
+++ src/br/com/alura/teste/collections/testaCopia.kt
@@ -0,0 +1,12 @@
+package br.com.alura.teste.array
+
+import br.com.alura.modelo.BancoDeNomes
+
+fun testaCopia(){
+    val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
+    // banco.nomes.add("Alex"
+    banco.salva("Alex")
+    println(nomesSalvos)
+    println(BancoDeNomes().nomes)
+}
