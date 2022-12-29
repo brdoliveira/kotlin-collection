8ecbf7c06b77f2ed90dcabd3bac3a432fc479c50
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-28 23:43:39 -0300

    FEAT(testaSet): Criando teste testeSet

--- src/br/com/alura/teste/collections/testaSet.kt
+++ src/br/com/alura/teste/collections/testaSet.kt
@@ -1,2 +1,25 @@
 package br.com.alura.teste.collections
 
+fun testaSet(){
+    val assistiramCursoAndroid : Set<String> = setOf("Alex","Fran","Gui","Maria")
+    val assistiramCursoKotlin : Set<String> = setOf("Alex","Paulo","Maria")
+//    val assistiramAmbos : Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
+
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid union assistiramCursoKotlin) // infix
+
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoKotlin subtract assistiramCursoAndroid)
+
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
+}
